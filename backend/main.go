ab91fc077910b1b5cf9c9af3b8a7ce821a1b7ec2
Jayden Chan <jaydencn7@gmail.com>
2022-01-15 17:08:54 -0800

    JC: Implement basic FACEIT demo parser

--- backend/main.go
+++ backend/main.go
@@ -18,6 +18,13 @@ func main() {
 	p := dem.NewParser(f)
 	defer p.Close()
 
+	faceitMode := true
+
+	kills := make(map[string]int)
+	deaths := make(map[string]int)
+	assists := make(map[string]int)
+	pastRoundTimes := [4]int{0, 0, 0, 0}
+
 	// Register handler on kill events
 	p.RegisterEventHandler(func(e events.Kill) {
 		var hs string
@@ -28,12 +35,57 @@ func main() {
 		if e.PenetratedObjects > 0 {
 			wallBang = " (WB)"
 		}
-		fmt.Printf("%s <%v%s%s> %s\n", e.Killer, e.Weapon, hs, wallBang, e.Victim)
+
+		var assister string
+		if e.Assister != nil {
+			assister = fmt.Sprintf(" (+%s)", e.Assister.Name)
+		}
+
+		if e.Assister != nil {
+			assists[e.Assister.Name] += 1
+		}
+
+		if e.Killer != nil {
+			kills[e.Killer.Name] += 1
+		}
+
+		if e.Victim != nil {
+			deaths[e.Victim.Name] += 1
+		}
+
+		fmt.Printf("%s <%v%s%s%s> %s\n", e.Killer, e.Weapon, assister, hs, wallBang, e.Victim)
+		if e.Killer != nil && e.Killer.Name == "" {
+			fmt.Printf("%s <%v%s%s%s> %s\n", e.Killer, e.Weapon, assister, hs, wallBang, e.Victim)
+		}
 	})
 
-	// Parse to end
+	p.RegisterEventHandler(func(e events.RoundEnd) {
+		fmt.Printf("%d %d - %d %d\n", e.WinnerState.ID(), e.WinnerState.Score() + 1, e.LoserState.Score(), e.LoserState.ID())
+	})
+
+	// Discard the knife round, warmup round, and triple-reset rounds
+	if faceitMode {
+		p.RegisterEventHandler(func(e events.RoundStart) {
+			pastRoundTimes[0] = pastRoundTimes[1]
+			pastRoundTimes[1] = pastRoundTimes[2]
+			pastRoundTimes[2] = pastRoundTimes[3]
+			pastRoundTimes[3] = e.TimeLimit
+
+			if pastRoundTimes[0] == 999 {
+				fmt.Println("RESETTING")
+				kills = make(map[string]int)
+				deaths = make(map[string]int)
+				assists = make(map[string]int)
+			}
+		})
+	}
+
 	err = p.ParseToEnd()
 	if err != nil {
 		panic(err)
 	}
+
+	fmt.Println("Kills", kills)
+	fmt.Println("Assists", assists)
+	fmt.Println("Deaths", deaths)
 }
