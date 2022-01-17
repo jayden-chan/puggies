4093497546bfd69c69a348ececf9ed9e98aafd74
Jayden Chan <jaydencn7@gmail.com>
2022-01-16 18:12:53 -0800

    JC: Make parser compatible with pugsetup demos

--- backend/main.go
+++ backend/main.go
@@ -2,15 +2,34 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"math"
+	"os"
 
 	dem "github.com/markus-wa/demoinfocs-golang/v2/pkg/demoinfocs"
 	events "github.com/markus-wa/demoinfocs-golang/v2/pkg/demoinfocs/events"
 )
 
+func mapValTotal(m *map[string]int) int {
+	sum := 0
+	for _, val := range *m {
+		sum += val
+	}
+	return sum
+}
+
+func arrayMapTotal(a *[]map[string]int) map[string]int {
+	ret := make(map[string]int)
+	for _, m := range *a {
+		for k, v := range m {
+			ret[k] += v
+		}
+	}
+	return ret
+}
+
 func main() {
 	f, err := os.Open("/home/jayden/Downloads/1-349fcf3c-681b-47e6-a134-47c8e27a25d9-1-1.dem")
+	// f, err := os.Open("/home/jayden/Downloads/pug_de_nuke_2022-01-16_05.dem")
 	if err != nil {
 		panic(err)
 	}
@@ -19,20 +38,21 @@ func main() {
 	p := dem.NewParser(f)
 	defer p.Close()
 
-	faceitMode := true
-	isGameLive := !faceitMode
-	totalRounds := 0
-
-	kills := make(map[string]int)
-	deaths := make(map[string]int)
-	assists := make(map[string]int)
-	headshots := make(map[string]int)
-	damage := make(map[string]int)
-
-	pastRoundTimes := [4]int{0, 0, 0, 0}
+	var kills []map[string]int
+	var deaths []map[string]int
+	var assists []map[string]int
+	var headshots []map[string]int
+	var damage []map[string]int
 
 	// Register handler on kill events
 	p.RegisterEventHandler(func(e events.Kill) {
+		// In faceit these events can sometimes trigger before we
+		// even have a RoundStart event so the stats arrays will be
+		// empty
+		if len(kills) == 0 {
+			return
+		}
+
 		var hs string
 		if e.IsHeadshot {
 			hs = " (HS)"
@@ -48,19 +68,19 @@ func main() {
 		}
 
 		if e.Assister != nil {
-			assists[e.Assister.Name] += 1
+			assists[len(assists)-1][e.Assister.Name] += 1
 		}
 
 		if e.Killer != nil {
-			kills[e.Killer.Name] += 1
+			kills[len(kills)-1][e.Killer.Name] += 1
 
 			if e.IsHeadshot {
-				headshots[e.Killer.Name] += 1
+				headshots[len(headshots)-1][e.Killer.Name] += 1
 			}
 		}
 
 		if e.Victim != nil {
-			deaths[e.Victim.Name] += 1
+			deaths[len(deaths)-1][e.Victim.Name] += 1
 		}
 
 		// fmt.Printf("%s <%v%s%s%s> %s\n", e.Killer, e.Weapon, assister, hs, wallBang, e.Victim)
@@ -74,41 +94,61 @@ func main() {
 	// })
 
 	p.RegisterEventHandler(func(e events.PlayerHurt) {
+		// In faceit these events can sometimes trigger before we
+		// even have a RoundStart event so the damage array will be
+		// empty
+		if len(damage) == 0 {
+			return
+		}
+
 		if e.Attacker != nil && e.Player != nil {
-			damage[e.Attacker.Name] += e.HealthDamageTaken
+			damage[len(damage)-1][e.Attacker.Name] += e.HealthDamageTaken
 		}
 	})
 
-	// Discard the knife round, warmup round, and triple-reset rounds
-	if faceitMode {
-		p.RegisterEventHandler(func(e events.RoundStart) {
-			// The start of a faceit match is signaled by one round
-			// with TimeLimit 999 followed by 3 restarts
-			pastRoundTimes[0] = pastRoundTimes[1]
-			pastRoundTimes[1] = pastRoundTimes[2]
-			pastRoundTimes[2] = pastRoundTimes[3]
-			pastRoundTimes[3] = e.TimeLimit
+	// Create a new 'round' map in each of the stats arrays
+	p.RegisterEventHandler(func(e events.RoundStart) {
+		kills = append(kills, make(map[string]int))
+		deaths = append(deaths, make(map[string]int))
+		assists = append(assists, make(map[string]int))
+		headshots = append(headshots, make(map[string]int))
+		damage = append(damage, make(map[string]int))
+	})
 
-			if pastRoundTimes[0] == 999 {
-				fmt.Println("----------------------------- Match is starting -----------------------------")
-				isGameLive = true
-				kills = make(map[string]int)
-				deaths = make(map[string]int)
-				assists = make(map[string]int)
-				headshots = make(map[string]int)
-				damage = make(map[string]int)
-			}
-
-			if isGameLive {
-				totalRounds += 1
-			}
-		})
-	}
-
+	fmt.Println("Parsing demo...")
 	err = p.ParseToEnd()
 	if err != nil {
 		panic(err)
 	}
+
+	// Figure out where the game actually goes live
+	startRound := 0
+	for i := len(kills) - 2; i > 0; i-- {
+		killsNext := mapValTotal(&kills[i+1])
+		killsCurr := mapValTotal(&kills[i])
+		killsPrev := mapValTotal(&kills[i-1])
+
+		// Three consecutive rounds with no kills will be
+		// considered the start of the game (faceit + pugsetup
+		// have the triple-restart and then MATCH IS LIVE thing)
+		if killsPrev+killsCurr+killsNext == 0 {
+			startRound = i + 1
+		}
+	}
+
+	kills = kills[startRound+1:]
+	deaths = deaths[startRound+1:]
+	assists = assists[startRound+1:]
+	headshots = headshots[startRound+1:]
+	damage = damage[startRound+1:]
+
+	totalRounds := len(kills)
+
+	totalKills := arrayMapTotal(&kills)
+	totalDeaths := arrayMapTotal(&deaths)
+	totalAssists := arrayMapTotal(&assists)
+	totalHeadshots := arrayMapTotal(&headshots)
+	totalDamage := arrayMapTotal(&damage)
 
 	// Initialze these maps with all players from kills + deaths
 	// in case anyone got 0 kills or 0 deaths (lol)
@@ -118,24 +158,24 @@ func main() {
 	headshotPct := make(map[string]float64)
 	adr := make(map[string]float64)
 
-	for p := range kills {
+	for p := range totalKills {
 		kd[p] = 0
 		kdiff[p] = 0
 		kpr[p] = 0
 		headshotPct[p] = 0
 	}
 
-	for p := range deaths {
+	for p := range totalDeaths {
 		kd[p] = 0
 		kdiff[p] = 0
 		kpr[p] = 0
 		headshotPct[p] = 0
 	}
 
-	// Compute headshot percentages, K/D & K-D
-	for player, numKills := range kills {
-		numHeadshots := headshots[player]
-		numDeaths := deaths[player]
+	// Compute headshot percentages, K/D & K-D etc
+	for player, numKills := range totalKills {
+		numHeadshots := totalHeadshots[player]
+		numDeaths := totalDeaths[player]
 
 		if numHeadshots == 0 || numKills == 0 {
 			headshotPct[player] = 0
@@ -146,26 +186,26 @@ func main() {
 		if numDeaths == 0 {
 			kd[player] = math.Inf(1)
 		} else {
-			kd[player] = math.Round((float64(numKills) / float64(numDeaths)) * 100) / 100
+			kd[player] = math.Round((float64(numKills)/float64(numDeaths))*100) / 100
 		}
 
 		kdiff[player] = numKills - numDeaths
-		kpr[player] = math.Round((float64(numKills) / float64(totalRounds)) * 100) / 100
+		kpr[player] = math.Round((float64(numKills)/float64(totalRounds))*100) / 100
 	}
 
-	for player, damage := range damage {
+	for player, damage := range totalDamage {
 		adr[player] = math.Round((float64(damage) / float64(totalRounds)))
 	}
 
 	fmt.Println()
 	fmt.Println("Total Rounds", totalRounds)
-	fmt.Println("Kills", kills)
-	fmt.Println("Assists", assists)
-	fmt.Println("Deaths", deaths)
-	fmt.Println("Headshot PCT", headshots)
+	fmt.Println("Kills", totalKills)
+	fmt.Println("Assists", totalAssists)
+	fmt.Println("Deaths", totalDeaths)
+	fmt.Println("Headshot PCT", headshotPct)
 	fmt.Println("K/D", kd)
 	fmt.Println("K-D", kdiff)
 	fmt.Println("KPR", kpr)
-	fmt.Println("Damage", damage)
+	fmt.Println("Damage", totalDamage)
 	fmt.Println("ADR", adr)
 }
