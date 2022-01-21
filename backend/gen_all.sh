519a3f507d2ef38c9dae19a39ab0c6c6102ff744
Jayden Chan <jaydencn7@gmail.com>
2022-01-20 17:24:31 -0800

    JC: Allow specifying demos path in gen_all.sh

--- backend/gen_all.sh
+++ backend/gen_all.sh
@@ -1,6 +1,8 @@
 #!/bin/zsh
 
-files=(../demos/*.dem)
+DEMOS_PATH=${DEMOS_PATH:-../demos}
+
+files=($DEMOS_PATH/pug_*.dem)
 for f in $files; do
     go run main.go $f > ../frontend/src/matchData/${f:t:r}.json
 done
